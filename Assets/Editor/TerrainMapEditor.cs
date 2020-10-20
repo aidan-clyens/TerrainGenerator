ddafea6f9a935891b4a024bc233c3935f1f4bbd6
Aidan Clyens <aidanc@rogers.com>
2020-10-19 22:00:43 -0400

    Update terrain map generator editor

--- Assets/Editor/TerrainMapEditor.cs
+++ Assets/Editor/TerrainMapEditor.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -13,8 +13,40 @@ public class TerrainMapEditor : Editor {
     public override void OnInspectorGUI() {
         TerrainMapGenerator terrainMapGenerator = (TerrainMapGenerator) target;
 
-        DrawDefaultInspector();
+        // Generator Settings
+        EditorGUILayout.LabelField("Generator Settings", EditorStyles.boldLabel);
+        terrainMapGenerator.seed = EditorGUILayout.IntField("Seed", terrainMapGenerator.seed);
+        terrainMapGenerator.mapWidth = EditorGUILayout.IntField("Map Width", terrainMapGenerator.mapWidth);
 
+        // Terrain Settings
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        EditorGUILayout.LabelField("Terrain Settings", EditorStyles.boldLabel);
+        terrainMapGenerator.terrainColourGradient = EditorGUILayout.GradientField("Terrain Colour Gradient", terrainMapGenerator.terrainColourGradient);
+        terrainMapGenerator.terrainMaterial = (Material)EditorGUILayout.ObjectField("Terrain Material", terrainMapGenerator.terrainMaterial, typeof(Material));
+        terrainMapGenerator.useFalloff = EditorGUILayout.Toggle("Use Falloff", terrainMapGenerator.useFalloff);
+        terrainMapGenerator.useHydraulicErosion = EditorGUILayout.Toggle("Use Hydraulic Erosion", terrainMapGenerator.useHydraulicErosion);
+        terrainMapGenerator.createForest = EditorGUILayout.Toggle("Create Forest", terrainMapGenerator.createForest);
+
+        // Perlin Noise Height Map Settings
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        EditorGUILayout.LabelField("Perlin Noise Settings", EditorStyles.boldLabel);
+        terrainMapGenerator.mapDepth = EditorGUILayout.IntField("Map Depth", terrainMapGenerator.mapDepth);
+        terrainMapGenerator.noiseScale = EditorGUILayout.FloatField("Noise Scale", terrainMapGenerator.noiseScale);
+        terrainMapGenerator.noiseOctaves = EditorGUILayout.IntField("Noise Octaves", terrainMapGenerator.noiseOctaves);
+        terrainMapGenerator.persistence = EditorGUILayout.FloatField("Persistence", terrainMapGenerator.persistence);
+        terrainMapGenerator.lacunarity = EditorGUILayout.FloatField("Lacunarity", terrainMapGenerator.lacunarity);
+        terrainMapGenerator.noiseRedistributionFactor = EditorGUILayout.FloatField("Noise Redistribution Factor", terrainMapGenerator.noiseRedistributionFactor);
+        terrainMapGenerator.normalizeLocal = EditorGUILayout.Toggle("Normalize Local", terrainMapGenerator.normalizeLocal);
+
+        // Water Settings
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        EditorGUILayout.LabelField("Water Settings", EditorStyles.boldLabel);
+        terrainMapGenerator.createWater = EditorGUILayout.Toggle("Create Water", terrainMapGenerator.createWater);
+        terrainMapGenerator.waterMaterial = (Material)EditorGUILayout.ObjectField("Water Material", terrainMapGenerator.waterMaterial, typeof(Material));
+        terrainMapGenerator.waterLevel = EditorGUILayout.FloatField("Water Level", terrainMapGenerator.waterLevel);
+
+        // Buttons
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
         if (GUILayout.Button("Generate")) {
             terrainMapGenerator.Generate(loadAllObjects: true);
         }
